dfa8aa0ba5c2fd9cd2039d90eda9234c8124d924
Bevan Rudge <bevan@drupal.geek.nz>
2015-03-26 11:24:57 +1300

    Attempting a fix

--- .travis/install-wp.sh
+++ .travis/install-wp.sh
@@ -6,7 +6,6 @@
 # License: GPL3
 
 export WP_CORE_DIR=/tmp/wordpress
-export WP_TESTS_DIR=/tmp/wordpress-tests
 
 # Init database
 mysql -e 'CREATE DATABASE wordpress_test;' -uroot
@@ -15,11 +14,6 @@ mysql -e 'CREATE DATABASE wordpress_test;' -uroot
 wget -nv -O /tmp/wordpress.tar.gz https://github.com/WordPress/WordPress/tarball/$WP_VERSION
 mkdir -p $WP_CORE_DIR
 tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
-
-# Grab testing framework and config file
-svn co --quiet --ignore-externals http://unit-tests.svn.wordpress.org/trunk/ $WP_TESTS_DIR
-
-wget -nv -O $WP_TESTS_DIR/wp-tests-config.php https://raw.github.com/tierra/wordpress-plugin-tests/setup/wp-tests-config.php
 
 # Put various components in proper folders
 plugin_slug=$(basename $(pwd))
