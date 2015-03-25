5e5ae6322334b89964860ce590becb90163be1b5
Bevan Rudge <bevan@drupal.geek.nz>
2015-03-26 10:28:18 +1300

    Configure The test suite support files

---
+++ .build/wp-tests-config.local.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Environment-specific test suite settings.
+ *
+ * @see wp-tests-config.php
+ */
+
+define( 'DB_NAME', 'wordpress_test' );
+define( 'DB_USER', 'travis' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST', 'localhost' );
